f34ce0eebaaba1eeb65507189e21bc5f77fef2b7
Dheeraj Joshi <dheerajjoshi157@gmail.com>
2019-05-03 09:00:46 +0530

    Change in assignment 8 2nd may

--- js-assignment-master/TS/ass_8_TS/ass_8_TS.js
+++ js-assignment-master/TS/ass_8_TS/ass_8_TS.js
@@ -9,19 +9,19 @@ runtimeVal.question("Enter a number to find it's hex value: ", function (answer)
     var number = parseInt(answer);
     var hValue = "";
     while (number != 0) {
-        if (number == 9)
+        if (number % 16 <= 9)
             hValue += number % 16;
-        else if (number == 10)
+        else if (number % 16 == 10)
             hValue += "A";
-        else if (number == 11)
+        else if (number % 16 == 11)
             hValue += "B";
-        else if (number == 12)
+        else if (number % 16 == 12)
             hValue += "C";
-        else if (number == 13)
+        else if (number % 16 == 13)
             hValue += "D";
-        else if (number == 14)
+        else if (number % 16 == 14)
             hValue += "E";
-        else if (number == 15)
+        else if (number % 16 == 15)
             hValue += "F";
         number = Math.floor(number / 16);
     }
